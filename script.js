67e8a5324ac184e9dcca7ade58ee4b15338121c2
Rosina <rosinaaa@gmail.com>
2024-02-18 15:29:54 +0000

    js added

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+//Select all elements from the DOM
+const keyEqualTo = document.querySelector("#equalTo");
+const keyPlus = document.querySelector("#plus");
+const keyDot = document.querySelector("#dot");
+const keyMinus = document.querySelector("#minus");
+const keyTimes = document.querySelector("#times");
+//All numbers
+const keyNumber = document.querySelectorAll(".numbers");
+//All operators
+const keyOperators = document.querySelectorAll(".operator");
